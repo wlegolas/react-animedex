ab2c50a3eedb0ebc222cec4a177002fcf08c5b68
Weslley Oliveira <wesshead@gmail.com>
2018-12-30 17:18:29 -0200

    Change base service to create a new instance for axios to use it as base for all requests

--- src/services/base.js
+++ src/services/base.js
@@ -1,9 +1,15 @@
-const baseUrl = 'https://kitsu.io/api/edge/';
-const combineUrl = urlPath => `${baseUrl}/${urlPath}`;
+import axios from 'axios';
+
+const request = axios.create({
+  baseURL: 'https://kitsu.io/api/edge/',
+  timeout: 1000
+});
+
+const isSuccessfully = status => status >= 200 && status < 300;
 
 const onRequestComplete = response => {
-  if (response.ok) {
-    return response.json();
+  if (isSuccessfully(response.status)) {
+    return response.data;
   }
 
   throw new Error(
@@ -11,19 +17,17 @@ const onRequestComplete = response => {
   );
 };
 
-const onRequestFailure = response => {
+const onRequestFailure = error => {
+  console.error(error);
   throw new Error(
-    `The server returns the status code: ${response.statusText}`
+    `The server returns the status code: ${error.response.status}`
   );
 };
 
 export const get = async (urlPath) => {
-  const options = { method: 'GET', mode: 'cors', cache: 'default' };
-  const url = combineUrl(urlPath);
-
-  return fetch(url, options)
+  return request.get(urlPath)
     .then(onRequestComplete)
     .catch(onRequestFailure);
 }
 
-export default { combineUrl, get };
+export default { get };
