7063dcbc2c813d20d34afe1e513a90d5a0274c3a
Weslley Oliveira <wesshead@gmail.com>
2019-03-24 18:16:07 -0300

    Remove Header componente froom ant lib

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
+import React, { Component, Fragment } from 'react';
 import { Layout } from 'antd';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
 import AnimeList from 'components/anime-list';
-import React, { Component } from 'react';
 import './App.css';
 
-const { Header, Content } = Layout;
+const { Content } = Layout;
 
 class App extends Component {
   render() {
     return (
-      <Layout>
-        <Header className="x-header">
-          <h1>Animedex - An easy way to discovery new animes</h1>
-        </Header>
+      <Fragment>
+        <header className="x-anime-header">
+          <AppBar position="static" color="primary">
+            <Toolbar>
+              <Typography variant="h6" color="inherit" className="x-anime-header--title">
+                Animedex - An easy way to discovery new animes
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </header>
         <Content className="x-content">
           <AnimeList />
         </Content>
-      </Layout>
+      </Fragment>
     );
   }
 }
